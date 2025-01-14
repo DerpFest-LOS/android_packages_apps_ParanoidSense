d8d9e11159bf5bf738e87aed3284b12b4e1df3f2
Jyotiraditya Panda <jyotiraditya@aospa.co>
2025-01-14 14:59:44 +0000

    ParanoidSense: Migrate to AndroidX Preference Manager
    Change-Id: Ib732162b5607ad702d44add3f22a0a2abc23d63d
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- src/co/aospa/sense/util/PreferenceHelper.kt
+++ src/co/aospa/sense/util/PreferenceHelper.kt
@@ -1,73 +1,70 @@
 package co.aospa.sense.util
 
-import android.annotation.SuppressLint
 import android.content.Context
-import android.preference.PreferenceManager
 import android.text.TextUtils
+import androidx.preference.PreferenceManager
 
 class PreferenceHelper(context: Context) {
 
-    private val mContext: Context
+    private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(
+        context.applicationContext
+    )
 
-    @SuppressLint("ApplySharedPref")
+    @Suppress("DEPRECATION")
     fun saveIntValue(key: String?, value: Int) {
-        val edit = PreferenceManager.getDefaultSharedPreferences(mContext).edit()
-        edit.putInt(key, value)
-        edit.commit()
+        sharedPreferences.edit()
+            .putInt(key, value)
+            .commit()
     }
 
-    @SuppressLint("ApplySharedPref")
+    @Suppress("DEPRECATION")
     fun saveBooleanValue(key: String?, value: Boolean) {
-        val edit = PreferenceManager.getDefaultSharedPreferences(mContext).edit()
-        edit.putBoolean(key, value)
-        edit.commit()
+        sharedPreferences.edit()
+            .putBoolean(key, value)
+            .commit()
     }
 
-    @SuppressLint("ApplySharedPref")
+    @Suppress("DEPRECATION")
     fun removeSharePreferences(value: String?) {
-        val edit = PreferenceManager.getDefaultSharedPreferences(mContext).edit()
-        edit.remove(value)
-        edit.commit()
+        sharedPreferences.edit()
+            .remove(value)
+            .commit()
     }
 
-    fun getIntValueByKey(key: String?, value: Int): Int {
-        return PreferenceManager.getDefaultSharedPreferences(mContext).getInt(key, value)
+    fun getIntValueByKey(key: String?, value: Int = -1): Int {
+        return sharedPreferences.getInt(key, value)
     }
 
-    fun getIntValueByKey(key: String?): Int {
-        return getIntValueByKey(key, -1)
-    }
-
-    @SuppressLint("ApplySharedPref")
+    @Suppress("DEPRECATION")
     fun saveStringValue(key: String?, value: String?) {
-        val edit = PreferenceManager.getDefaultSharedPreferences(mContext).edit()
-        edit.putString(key, value)
-        edit.commit()
+        sharedPreferences.edit()
+            .putString(key, value)
+            .commit()
     }
 
     fun getStringValueByKey(key: String?): String? {
-        return PreferenceManager.getDefaultSharedPreferences(mContext).getString(key, null)
+        return sharedPreferences.getString(key, null)
     }
 
-    @SuppressLint("ApplySharedPref")
+    @Suppress("DEPRECATION")
     fun saveByteArrayValue(key: String?, value: ByteArray?) {
-        val edit = PreferenceManager.getDefaultSharedPreferences(mContext).edit()
-        edit.putString(key, String(value!!))
-        edit.commit()
+        requireNotNull(value) { "ByteArray value cannot be null" }
+
+        sharedPreferences.edit()
+            .putString(key, String(value))
+            .commit()
     }
 
     fun getByteArrayValueByKey(key: String?): ByteArray? {
-        val string = PreferenceManager.getDefaultSharedPreferences(mContext).getString(key, null)
-        return if (TextUtils.isEmpty(string)) {
+        val string = sharedPreferences.getString(key, null)
+        return if (string.isNullOrEmpty()) {
             null
-        } else string!!.toByteArray()
+        } else {
+            string.toByteArray()
+        }
     }
 
     fun getBooleanValueByKey(key: String?): Boolean {
-        return PreferenceManager.getDefaultSharedPreferences(mContext).getBoolean(key, false)
-    }
-
-    init {
-        mContext = context.applicationContext
+        return sharedPreferences.getBoolean(key, false)
     }
 }
